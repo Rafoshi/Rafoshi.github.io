641bb572ae0c2ece82eff0be29f31edb744bb955
Rafael  Ioshi Imamura Pereira <rafa.ioshi@gmail.com>
2020-12-16 16:13:11 -0300

    Update

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -4,6 +4,35 @@ let show = true;
 const menuSection = document.querySelector(".menu-section")
 const menuToggle = menuSection.querySelector(".menu-toggle")
 
+const home = document.querySelector(".home")
+const menu = document.querySelector(".menu")
+const deposition = document.querySelector(".deposition")
+const about = document.querySelector(".about")
+
+about.addEventListener("click", () => {
+
+  document.body.style.overflow = show ? "hidden" : "initial"
+
+  menuSection.classList.toggle("on", show)
+  show = !show;
+})
+
+deposition.addEventListener("click", () => {
+
+  document.body.style.overflow = show ? "hidden" : "initial"
+
+  menuSection.classList.toggle("on", show)
+  show = !show;
+})
+
+home.addEventListener("click", () => {
+
+  document.body.style.overflow = show ? "hidden" : "initial"
+
+  menuSection.classList.toggle("on", show)
+  show = !show;
+})
+
 menuToggle.addEventListener("click", () => {
 
     document.body.style.overflow = show ? "hidden" : "initial"
@@ -127,3 +156,4 @@ function slide(wrapper, items, prev, next) {
 }
 
 slide(slider, sliderItems, prev, next);
+
